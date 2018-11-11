61da71d65613e5182f60ce15d787c6c5978bf18f
YouGeniusGames <43735102+YouGeniusGames@users.noreply.github.com>
2018-11-11 23:35:35 +1300

    Add files via upload
    unity scripts

---
+++ PlayerShooting.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+    public Vector3 bulletOffset = new Vector3(0, 0.5f, 0);
+
+    public float fireDelay = 0.25f;
+    float cooldownTimer = 0;
+	void Update () {
+        cooldownTimer -= Time.deltaTime;
+        if (Input.GetButton("Fire1") && cooldownTimer <= 0)
+        {
+            Debug.Log("shooting");
+            cooldownTimer = fireDelay;
+
+            Vector3 offset = transform.rotation * bulletOffset;
+
+            Instantiate(bulletPrefab, transform.position, transform.rotation);
+        }
+	}
+}
